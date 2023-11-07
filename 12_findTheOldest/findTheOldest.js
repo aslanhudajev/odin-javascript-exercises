5610e033bbaae5005bd3dcb749813d9dcaadee7a
Aslan <aslambek.hudajev@gmail.com>
2023-11-07 20:33:17 +0100

    solved 8, 9, 19 and 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+function CalcAge(deathYear, birthYear)
+{
+    if(!deathYear) deathYear = new Date().getFullYear();
+    return deathYear - birthYear;
+}
 
+const findTheOldest = function(people) {
+    return people.sort((curr, next) => {
+        let currAge = CalcAge(curr.yearOfDeath, curr.yearOfBirth);
+        let nextAge = CalcAge(next.yearOfDeath, next.yearOfBirth);
+
+        if(currAge < nextAge){
+            return 1;
+        }
+        else if(currAge > nextAge){
+            return -1;
+        }
+
+        return 0;
+    })[0];
 };
 
 // Do not edit below this line
